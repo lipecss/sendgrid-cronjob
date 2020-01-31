6c597a2206ab2ea1a64f7b51c1f5abbcad1059da
Felipecss <felipecss@vemlavaralouca.com.br>
2020-01-30 22:11:51 -0300

    make it better

--- src/index.js
+++ src/index.js
@@ -1,21 +1,81 @@
 const schedule = require('node-schedule')
+const sgMail = require('@sendgrid/mail')
 const axios = require('axios')
+const moment = require('moment')
 
+const sendGridKey = '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'
+
+sgMail.setApiKey(sendGridKey)
+
+const users = [
+    {
+        'name': 'Carlos',
+        'email': 'carlinhos@bb20.com.br',
+        'password': '123456789',
+        'birthday': moment('1980-02-15', "YYYY-MM-DD")
+    },
+    {
+        'name': 'Joana',
+        'email': 'felipecss@vemlavaralouca.com.br',
+        'password': '123456789',
+        'birthday': moment('1994-01-31', "YYYY-MM-DD")
+    },
+    {
+        'name': 'Lucas',
+        'email': 'felipeszxbox@gmail.com',
+        'password': '123456789',
+        'birthday': moment('1950-01-31', "YYYY-MM-DD")
+    },
+    {
+        'name': 'Marcos',
+        'email': 'marcos_arruda@hotmail.com,',
+        'password': '123456789',
+        'birthday': moment('1950-02-30', "YYYY-MM-DD")
+    },
+    {
+        'name': 'Camila',
+        'email': 'gg_game@gmail.com',
+        'password': '123456789',
+        'birthday': moment('2006-12-25', "YYYY-MM-DD")
+    },
+    {
+        'name': 'Jessica',
+        'email': 'Jeje_loirinha@live.com,',
+        'password': '123456789',
+        'birthday': moment('2002-08-05', "YYYY-MM-DD")
+    }
+    
+]
+    
+   
 var rule = new schedule.RecurrenceRule()
-    rule.minute = new schedule.Range(0, 60, 5)
+    // Regra que será executada todo dias as 07:00
+    rule.dayOfWeek = new schedule.Range(0, 6)
+    rule.hour = 7
+    rule.minute = 0
 
-let job = schedule.scheduleJob(rule, async function(){
-    await axios.get('https://omnistack-10.herokuapp.com/devs')
-        .then(response => {
-            response.data.map(dev => {
-                let data = {
-                    'name': dev.name,
-                    'github': dev.github_username
-                }
+let job = schedule.scheduleJob(rule, async function() {
 
-                console.log(data)
-            })
-        }).catch(err => {
-            console.log(err)
-        })
+    const today = moment().format("MM-DD");
+
+    const isBirthday = users.filter((item, index) => {
+        return item.birthday.format("MM-DD") === today
+    })
+
+    isBirthday.forEach(dev => {
+        sendEmail(dev)
+    })
 })
+
+
+function sendEmail(dev) {
+    let msg = {
+        to: dev.email,
+        from: 'lipegame360@live.com',
+        subject: 'Today is your Day!!!!',
+        text: `Olá ${dev.name} hoje é um dia especial, é o seu dia! Nos da Vem lavar a louça te desejamos muitas felicidades`,
+        html: `<strong>Olá ${dev.name} hoje é um dia especial, é o seu dia!</strong>`
+    }
+    console.log('FOI')
+    sgMail.send(msg)
+}
